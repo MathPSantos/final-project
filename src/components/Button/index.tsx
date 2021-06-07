498da5ac59df58a87e252c38a388733492c44f62
MathPSantos <matheus.psantos2016@gmail.com>
2021-06-07 16:48:15 -0300

    feat: add components structure

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.scss'
+
+export function Button() {
+  return <h1>Button</h1>
+}
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.scss'
+
+export function Input() {
+  return <h1>Input</h1>
+}
